5cd1fbef67d15e58acb899e4b6049ff191ca48f5
Mike Wagner <mvw5669@psu.edu>
2021-10-29 14:00:42 -0400

    icon displays and banner is layed out properly

--- src/learning-banner.js
+++ src/learning-banner.js
@@ -1,90 +1,89 @@
 import { html, css } from 'lit';
-import { SimpleColors } from "@lrnwebcomponents/simple-colors/simple-colors.js";
-import "./learning-icon.js";
-import "./learning-header.js";
+import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
+import './learning-icon.js';
+import './learning-header.js';
 
 export class LearningBanner extends SimpleColors {
-    static get tag() {
-        return 'learning-banner';
-    }
+  static get tag() {
+    return 'learning-banner';
+  }
 
-    static get properties() {
-        return {
-            ...super.properties,
-            type: { type: String },
-        };
-    }
+  static get properties() {
+    return {
+      ...super.properties,
+      type: { type: String },
+    };
+  }
 
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.accentColor = 'blue';
+    this.type = null;
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === 'type' && this[propName] === 'idea') {
+        this.accentColor = 'orange';
+        this.type = 'idea';
+      } else if (propName === 'type' && this[propName] === 'question') {
         this.accentColor = 'blue';
-        this.type = null;
-    }
+        this.type = 'question';
+      } else if (propName === 'type' && this[propName] === 'science') {
+        this.accentColor = 'green';
+        this.type = 'science';
+      }
+    });
+  }
 
-    updated(changedProperties) {
-        changedProperties.forEach((oldValue, propName) => {
-            if (propName === "type" && this[propName] === "idea") {
-                this.accentColor = "orange";
-                this.type = 'idea';
-            }
-            else if (propName === "type" && this[propName] === "question") {
-                this.accentColor = "blue";
-                this.type = 'question';
-            }
-            else if (propName === "type" && this[propName] === "science") {
-                this.accentColor = "green";
-                this.type = 'science';
-            }
-        });
-    }
+  static get styles() {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          display: flex;
+          flex-direction: row;
+          background-color: var(--simple-colors-default-theme-accent-1);
+          border: 2px solid blue;
+        }
 
-    static get styles() {
-        return [...super.styles,
-            css`
-            :host {
-                display: flex;
-                flex-direction: row;
-                background-color: var(--simple-colors-default-theme-accent-1);
-                border: 2px solid blue;
-            }
+        :host([type='idea']) {
+          background-color: var(--simple-colors-default-theme-accent-5);
+        }
+        :host([type='question']) {
+          background-color: var(--simple-colors-default-theme-accent-7);
+        }
+        :host([type='science']) {
+          background-color: var(--simple-colors-default-theme-accent-7);
+        }
 
-            
-            :host([type='idea']) {
-                background-color: var(--simple-colors-default-theme-accent-5);
-            }
-            :host([type='question']) {
-                background-color: var(--simple-colors-default-theme-accent-7);
-            }
-            :host([type='science']) {
-                background-color: var(--simple-colors-default-theme-accent-7);
-            }
+        .banner {
+          display: flex;
+          width: 700px;
+        }
 
-            .banner{ 
-                width: auto;
-            }
+        .iconArea {
+          width: 15%;
+        }
 
-            .iconArea{
-                width: 15%;
-            }
+        .headerArea {
+          width: 85%;
+        }
+      `,
+    ];
+  }
 
-            .headerArea{
-                width: 85%;
-            }
-            `
-        ];
-    }
-    render() {
-        return html
-        `
-        <div class="banner">
-            <div>
-                <learning-icon type="${this.type}"></learning-icon>
-            </div>
-            <div class="headerArea">
-                <slot name="header"></slot>
-            </div>
+  render() {
+    return html`
+      <div class="banner">
+        <div>
+          <learning-icon type="${this.type}"></learning-icon>
         </div>
-        `;
-    }
+        <div class="headerArea">
+          <slot name="header"></slot>
+        </div>
+      </div>
+    `;
+  }
 }
 window.customElements.define(LearningBanner.tag, LearningBanner);
